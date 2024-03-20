33821c9ea51ee998a7fc7fd27c4911c5aa90f39c
Aguru Sireesha <agurusireeshaedu@gmail.com>
2024-03-20 06:11:45 +0000

    Match Game

---
+++ src/components/MatchGame/index.js
@@ -0,0 +1,56 @@
+import './index.css'
+import TabsCard from '../TabsCard'
+import MatchGameCard from '../MatchGameCard'
+
+const MatchGame = props => {
+  const {
+    allImagesList,
+    tabs,
+    thumbnailImages,
+    activeTabId,
+    currentImageId,
+    onClickTab,
+    onClickThumbnail,
+  } = props
+
+  // functions
+  const getImage = () =>
+    allImagesList.find(image => image.id === currentImageId)
+
+  const renderTabs = () => (
+    <ul className="tab-items-container">
+      {tabs.map(tab => (
+        <TabsCard
+          tabDetails={tab}
+          active={tab.tabId === activeTabId}
+          onClick={onClickTab}
+          key={tab.tabId}
+        />
+      ))}
+    </ul>
+  )
+  const renderThumbnails = () => (
+    <ul className="thumbnails-container">
+      {thumbnailImages.map(thumbnail => (
+        <MatchGameCard
+          thumbnail={thumbnail}
+          key={thumbnail.id}
+          onClickThumbnail={onClickThumbnail}
+        />
+      ))}
+    </ul>
+  )
+
+  // return code
+
+  const {imageUrl} = getImage()
+  return (
+    <div className="game-container">
+      <img className="match-image" src={imageUrl} alt="match" />
+      {renderTabs()}
+      {renderThumbnails()}
+    </div>
+  )
+}
+
+export default MatchGame
